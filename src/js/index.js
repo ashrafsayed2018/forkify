1663b21d504a23be58885d4fe0483e84e639f7af
unknown <ashrafsayed19841111@gmail.com>
2020-10-11 00:09:14 +0200

    modules import and export in es6

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 // Global app controller
-import num from "./test"; 
-const x = 230;
-console.log(`i imported from another module ${num} and x is ${x}`)
+
+import string from "./models/Search"
+// import {add , multiply ,ID} from "./views/searchView"
+// import {add as a, multiply as m,ID} from "./views/searchView"
+import * as searchView from "./views/searchView"
+console.log(`using imported function : ${searchView.add(searchView.ID,7)} and ${searchView.multiply(8,5)} , ${string}`)
